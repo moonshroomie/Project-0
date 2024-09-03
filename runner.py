40ad62796962e4644f1866f8474bb857c5e46d9a
Brooke <144165292+moonshroomie@users.noreply.github.com>
2024-09-03 18:05:03 +0000

    worked

--- runner.py
+++ runner.py
@@ -1,12 +1,14 @@
 from character import Character
 from item import Item
-from wordle import playWordle
+#from wordle import playWordle
 
-print("Welcome to the Game!")
-
-d1 = input("You come up to a field and see a star! Do you want to pick it up? (y/n)")
+print("Welcome to Walmart Goat Simulator!")
+print(""
+      "")
+d1 = input("You see a single mother walking down the sidewalk with her 3 year-old son. Do you headbutt her son into the stratosphere? (y/n)")
 if d1 == "y":
-    star = Item("Star", "A Shiny Star", 10)
+    child = Item("Child", "3 year-old", 10)
+    print("You headbutted the child into the stratosphere! Excellent work!")
     d1a = input ("You picked up the star! Yay! Now you see a dude. Want to talk to him? (y/n)")
     if d1a == "y":
         character = Character("Dude", 100, [], {"Hello": "Hey, I'm a dude!"})
